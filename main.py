a8813840c2a5b8bebfd77fdd79df6062c29bc9cb
Henry Vogel <henryhsvogel@web.de>
2024-01-03 21:01:29 +0100

    Änderung der Knotenfarbe nach Firma

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-# This is a sample Python script.
 from pyvis.network import Network
 import pandas as pd
 
 
 def parseList(param):
     output = []
-    if "|" not in param:
+    if "," not in param:
         output.append(param)
         return output
     else:
-        return param.split("|")
+        return param.split(",")
 
 
-if __name__ == '__main__':
+def setColor(param):
+    if param == "A":
+        return "blue"
+    elif param == "B":
+        return "green"
+
+
+if __name__ == "__main__":
     net = Network(notebook=True, cdn_resources="remote", select_menu=True, filter_menu=True)                    #Graph initialisieren
-    df = pd.read_csv('list.CSV', sep=';')                                                         #CSV-Datei wird ausgelesen
+    df = pd.read_csv("list.CSV", sep=";")                                                         #CSV-Datei wird ausgelesen
     df["Parent Node"].fillna("leer", inplace=True)                                                              #Leere Felder werden gefüllt
 
-    nameList = df['Name']                                                                                       #Zugriff auf Spalten
-    parentList = df['Parent Node']
-    indexList = df['Index']
+    nameList = df["Name"]                                                                                       #Zugriff auf Spalten
+    firmaList = df["Firma"]
+    bereichList = df["Fachbereich"]
+    personList = df["Verantwortlicher"]
+    parentList = df["Parent Node"]
+    indexList = df["Index"]
 
     for index in indexList:                                                                                      #Iteriere über Liste
         if parentList[index] == "leer":
-            net.add_node(nameList[index], title="Hauptprgramm", color="red")                                     #Fuege Hauptprogramme als rote Knoten ein
+            net.add_node(nameList[index], title="Hauptprogramm", color=setColor(firmaList[index]))               #Fuege Hauptprogramme mit Farbe je nach Firma ein
         else:
-            net.add_node(nameList[index], title="Unterprogramm von " + parentList[index], color="blue")          #Fuege Unterprogramme als blaue Knoten ein
+            net.add_node(nameList[index], title="Unterprogramm von " + parentList[index], color=setColor(firmaList[index]))          #Fuege Unterprogramme
 
     for index in indexList:
         for parents in parseList(parentList[index]):
             if parentList[index] != "leer":
                 net.add_edge(nameList[index], parents)                                                           #Fuege Kanten hinzu
-    net.show('test.html')                                                                                        #Output html-Datei
+    net.show("test.html")                                                                                        #Output html-Datei
 
 
